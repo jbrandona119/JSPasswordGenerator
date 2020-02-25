5c208cc4058f57a639ab38fac9d62bfaa32b55b5
Brandon Arnold <jbrandona119@gmail.com>
2020-02-24 23:28:21 -0500

    renamed html to index.html, added more javascript that generates random password. still need to add all the features, bootstrap and CSS.

--- script.js
+++ script.js
@@ -4,5 +4,20 @@ function generate() {
 
     //set pw length and details
 
-    let complexity = document.getElementById("slider").value
+    let complexity = document.getElementById("slider").value;
+
+    // password values
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+    
+    let password = "";
+
+    //create for loop to choose characters for password
+
+for (var i = 0; i <= complexity; i++) {
+    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+}
+
+    //add password to textbox area
+    document.getElementById("display").value = password;
+
 }
